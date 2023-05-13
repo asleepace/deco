8f751639bc4213de49d5d23530243b98fa5d0de8
Colin <colin_teahan@yahoo.com>
2023-05-13 13:15:18 -0700

    updating pure types

--- src/@pure/pure.ts
+++ src/@pure/pure.ts
@@ -3,18 +3,33 @@ import { throwOnSidEffects } from "./throwOnSideEffects"
  * This decorator will throw an error if side effects are detected on a method.
  */
 export function pure(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-  console.log({ propertyKey, descriptor })
-  console.log(Object.getOwnPropertyDescriptor(target, propertyKey))
+  // console.log({ propertyKey, descriptor })
+  // console.log(Object.getOwnPropertyDescriptor(target, propertyKey))
 
-  const targetMethod = target[propertyKey]
-  console.log(typeof targetMethod)
-  console.log({ targetMethod })
+  // const targetMethod = target[propertyKey]
+  // console.log(typeof targetMethod)
+  // console.log({ targetMethod })
 
-  if (typeof targetMethod !== 'function') {
-    throw new Error(`@pure decorator can only be used on methods: ${propertyKey} is not a method.`)
-  }
+  // // make sure th
+  // if (typeof targetMethod !== 'function') {
+  //   throw new Error(`@pure decorator can only be used on methods: ${propertyKey} is not a method.`)
+  // }
 
+  // overrides the methods "this" with a proxy that will throw if a mutation is made.
   return Object.defineProperty(target, propertyKey, {
-    value: throwOnSidEffects(targetMethod),
+    value: target[propertyKey].bind(new Proxy({}, {
+      get(target, propertyKey, receiver) {
+        console.warn('get', { target, propertyKey, receiver })
+        throw new Error(`@pure side effect detected (get) property: ${String(propertyKey)}`)
+      },
+      set(target, propertyKey, value, receiver) {
+        console.warn('get', { target, propertyKey, receiver })
+        throw new Error(`@pure side effect detected (set) value ${value} on property: ${String(propertyKey)}`)
+      },
+      apply(target, thisArg, argumentsList) {
+        console.warn('get', { target, thisArg, argumentsList })
+        throw new Error(`@pure side effect detected (apply) ${target} on: ${thisArg}`)
+      },
+    }))
   })
 };
--- src/@pure/throwOnSideEffects.ts
+++ src/@pure/throwOnSideEffects.ts
@@ -4,16 +4,52 @@
 export function throwOnSidEffects(targetMethod: any) {
   return targetMethod.bind(new Proxy({}, {
     get(target, propertyKey, receiver) {
-      console.warn('get', { target, propertyKey, receiver })
+      console.warn('@pure get', { target, propertyKey, receiver })
       throw new Error(`@pure side effect detected (get) property: ${String(propertyKey)}`)
     },
     set(target, propertyKey, value, receiver) {
-      console.warn('get', { target, propertyKey, receiver })
+      console.warn('@pure set', { target, propertyKey, receiver })
       throw new Error(`@pure side effect detected (set) value ${value} on property: ${String(propertyKey)}`)
     },
     apply(target, thisArg, argumentsList) {
-      console.warn('get', { target, thisArg, argumentsList })
+      console.warn('@pure apply', { target, thisArg, argumentsList })
       throw new Error(`@pure side effect detected (apply) ${target} on: ${thisArg}`)
     },
+    deleteProperty(target, propertyKey) {
+      console.warn('@pure deleteProperty', { target, propertyKey })
+      throw new Error(`@pure side effect detected (deleteProperty) ${target} on: ${String(propertyKey)}`)
+    },
+    defineProperty(target, propertyKey, descriptor) {
+      console.warn('@pure defineProperty', { target, propertyKey, descriptor })
+      throw new Error(`@pure side effect detected (defineProperty) ${target} on: ${String(propertyKey)}`)
+    },
+    getOwnPropertyDescriptor(target, propertyKey) {
+      console.warn('@pure getOwnPropertyDescriptor', { target, propertyKey })
+      throw new Error(`@pure side effect detected (getOwnPropertyDescriptor) ${target} on: ${String(propertyKey)}`)
+    },
+    getPrototypeOf(target) {
+      console.warn('@pure getPrototypeOf', { target })
+      throw new Error(`@pure side effect detected (getPrototypeOf) ${target}`)
+    },
+    has(target, propertyKey) {
+      console.warn('@pure has', { target, propertyKey })
+      throw new Error(`@pure side effect detected (has) ${target} on: ${String(propertyKey)}`)
+    },
+    isExtensible(target) {
+      console.warn('@pure isExtensible', { target })
+      throw new Error(`@pure side effect detected (isExtensible) ${target}`)
+    },
+    ownKeys(target) {
+      console.warn('@pure ownKeys', { target })
+      throw new Error(`@pure side effect detected (ownKeys) ${target}`)
+    },
+    preventExtensions(target) {
+      console.warn('@pure preventExtensions', { target })
+      throw new Error(`@pure side effect detected (preventExtensions) ${target}`)
+    },
+    setPrototypeOf(target, prototype) {
+      console.warn('@pure setPrototypeOf', { target, prototype })
+      throw new Error(`@pure side effect detected (setPrototypeOf) ${target}`)
+    }
   }))
 }
